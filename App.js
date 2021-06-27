e7f3fc7ac775fdaf809d759e154efe8a7be1b577
Ryan Morey <ryan@Mac-mini.local>
2021-06-27 12:51:30 -0400

    Add dummy search field and button

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, SafeAreaView} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SearchBar/>
+      <Button
+        title="Go"
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+const SearchBar = () => {
+  const [text, onChangeText] = React.useState(null);
+
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder="Enter search term"
+      />
+    </SafeAreaView>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
