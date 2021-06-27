824d25f63c71f00d580565788a6761c2dc40a0aa
Ryan Morey <ryan@Mac-mini.local>
2021-06-27 14:27:01 -0400

    prettify

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Pressable, StyleSheet, Text, View, Button, TextInput, SafeAreaView, FlatList, Image} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  SafeAreaView,
+  FlatList,
+  Image,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const API_KEY = '22259626-cf646f94d7bf37e93a1753150';
-
+const API_KEY = "22259626-cf646f94d7bf37e93a1753150";
 
 function DetailsScreen({ route, navigation }) {
   const item = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Image 
-          source={{uri: item.webformatURL}}
-          style={{flex:1, width: 100, height:100 }}
-          resizeMode="contain"
-
-          />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Posted by {item.user}</Text>
+      <Text>{item.tags}</Text>
+      <Image
+        source={{ uri: item.webformatURL }}
+        style={{ flex: 1, width: 100, height: 100 }}
+        resizeMode="contain"
+      />
     </View>
   );
 }
@@ -28,7 +38,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Search" component={SearchScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -36,61 +46,49 @@ function App() {
 
 export default App;
 
-
-
-
-
-
-
 function SearchScreen({ navigation }) {
   const [text, onChangeText] = React.useState(null);
   const [results, setResults] = React.useState([]);
   return (
     <View style={styles.container}>
       <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        placeholder="Enter search term"
-      />
-    </SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="Enter search term"
+        />
+      </SafeAreaView>
       <Button
         title="Go"
         disabled={!text}
-        onPress={()=>{
+        onPress={() => {
           fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${text}`)
-          .then(response => response.json())
-          .then(data => setResults(data))
-
+            .then((response) => response.json())
+            .then((data) => setResults(data));
         }}
       />
-            <FlatList
+      <FlatList
         data={results.hits}
-        renderItem={({item}) => 
-        <Pressable
-        onPress={() => navigation.navigate('Details',item)}
-        >
-        <Image 
-          source={{uri: item.previewURL}}
-          style={{width: 50, height: 50 }}
-          />
-        </Pressable>
-
-      }
+        renderItem={({ item }) => (
+          <Pressable onPress={() => navigation.navigate("Details", item)}>
+            <Image
+              source={{ uri: item.previewURL }}
+              style={{ width: 50, height: 50 }}
+            />
+          </Pressable>
+        )}
       />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     height: 40,
